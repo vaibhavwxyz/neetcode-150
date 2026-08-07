cca8abdc4ba8dd3d3bbfd9561edcb7de99d2b70c
vaibhawxyz <vaibhawxyz@gmail.com>
2026-08-07 23:06:00 +0530

    product-of-array-except-self

--- 01-arrays-and-hashing/0238-product-of-array-except-self.js
+++ 01-arrays-and-hashing/0238-product-of-array-except-self.js
@@ -8,7 +8,22 @@
  */
 
 function productExceptSelf(nums) {
-  // TODO
+  const n = nums.length;
+  const result = new Array(n);
+
+  let prefix = 1;
+  for (let i = 0; i < n; i++) {
+    result[i] = prefix;
+    prefix *= nums[i];
+  }
+
+  let suffix = 1;
+  for (let i = n - 1; i >= 0; i--) {
+    result[i] *= suffix;
+    suffix *= nums[i];
+  }
+
+  return result;
 }
 
 // --- tests ---
