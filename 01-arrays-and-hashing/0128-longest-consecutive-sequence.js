16e418b6dfc497ee7a2ba89e211fe25394f6b033
vaibhawxyz <vaibhawxyz@gmail.com>
2026-08-08 23:03:50 +0530

    longest-consecutive

--- 01-arrays-and-hashing/0128-longest-consecutive-sequence.js
+++ 01-arrays-and-hashing/0128-longest-consecutive-sequence.js
@@ -8,7 +8,20 @@
  */
 
 function longestConsecutive(nums) {
-  // TODO
+  const numSet = new Set(nums);
+  let longest = 0;
+
+  for (const num of numSet) {
+    if (!numSet.has(num - 1)) {
+      let length = 1;
+      while (numSet.has(num + length)) {
+        length++;
+      }
+      longest = Math.max(longest, length);
+    }
+  }
+
+  return longest;
 }
 
 // --- tests ---
