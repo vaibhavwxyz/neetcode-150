d919c1313eb79ae32ccf9e5e1bc79189aab8d8ec
vaibhawxyz <vaibhawxyz@gmail.com>
2026-08-10 22:53:27 +0530

    encode-and-decode-string

--- 01-arrays-and-hashing/0271-encode-and-decode-strings.js
+++ 01-arrays-and-hashing/0271-encode-and-decode-strings.js
@@ -8,11 +8,29 @@
  */
 
 function encode(strs) {
-  // TODO
+  let result = "";
+  for (const str of strs) {
+    result += str.length + "#" + str;
+  }
+  return result;
 }
 
 function decode(str) {
-  // TODO
+  const result = [];
+  let i = 0;
+
+  while (i < s.length) {
+    let j = i;
+    while (s[j] !== "#") {
+      j++;
+    }
+    const length = parseInt(s.slice(i, j), 10);
+    const str = s.slice(j + 1, j + 1 + length);
+    result.push(str);
+    i = j + 1 + length;
+  }
+
+  return result;
 }
 
 // --- tests ---
