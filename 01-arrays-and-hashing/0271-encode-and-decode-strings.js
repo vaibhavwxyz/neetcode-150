05712cc64f851b247f1dda50946127b4bf8128c9
vaibhawxyz <vaibhawxyz@gmail.com>
2026-08-11 23:23:58 +0530

    encode-decode

--- 01-arrays-and-hashing/0271-encode-and-decode-strings.js
+++ 01-arrays-and-hashing/0271-encode-and-decode-strings.js
@@ -2,9 +2,6 @@
  * 271. Encode and Decode Strings — Medium
  * https://leetcode.com/problems/encode-and-decode-strings/
  *
- * Approach:
- * Time:  O()
- * Space: O()
  */
 
 function encode(strs) {
